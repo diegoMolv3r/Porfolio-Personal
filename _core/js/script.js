e8751b118f72d0c026d352cc06be1b493acdf65d
Diego Molver <molverdiego@gmail.com>
2024-10-16 01:13:52 -0300

    Cambios finales 15-10

--- _core/js/script.js
+++ _core/js/script.js
@@ -1,5 +1,17 @@
 var header = document.getElementById('header-nav');
+var imagenOculta = document.getElementById('imagen-oculta');
 
-function desplegarNav(){
-    header.style.display="block";
+function navState(){
+    if(header.classList.contains('nav-desplegado')){
+        header.classList.remove('nav-desplegado');
+        header.classList.add('nav-replegado');
+        imagenOculta.style.display="block";
+
+    }else if(header.classList.contains('nav-replegado')){
+        header.classList.remove('nav-replegado');
+        header.classList.add('nav-desplegado');
+        imagenOculta.style.display="none";
+    }
 }
+
+
