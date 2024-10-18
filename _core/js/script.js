2c05ac5640189b3469b1c410f017e771f14cc97d
Diego Molver <molverdiego@gmail.com>
2024-10-17 22:26:29 -0300

    avatar

--- _core/js/script.js
+++ _core/js/script.js
@@ -1,11 +1,14 @@
 var header = document.getElementById('header-nav');
 var nav = document.getElementById('navegation-var');
+
 function navState(){
     if(header.classList.contains('animate__slideInLeft')){
         header.classList.remove('animate__slideInLeft');
         header.classList.add('animate__slideOutLeft');
+        
     }else if(header.classList.contains('animate__slideOutLeft')){
         header.classList.remove('animate__slideOutLeft');
         header.classList.add('animate__slideInLeft');
+        
     }
 }
