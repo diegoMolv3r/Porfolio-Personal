e23c3f4f5c18ffd6f38fc3f663c10800c6a67968
Diego Molver <molverdiego@gmail.com>
2025-07-23 17:15:36 -0300

    Renderizacion de proyectos con JSON

--- _core/js/script.js
+++ _core/js/script.js
@@ -12,3 +12,56 @@ function navState(){
         
     }
 }
+
+
+const proyectos = [
+    {
+        titulo: 'Gestion Bancaria',
+        tecnologias: ['Java', 'Junit'],
+        enlace: 'https://github.com/diegoMolv3r/Proyecto-Banco-PB2'
+    }
+]
+
+
+function renderizarProyectos(proyecto){
+    let article = document.createElement('article')
+    article.classList.add('card')
+
+    let titulo = document.createElement('h3')
+    titulo.classList.add('titulo-proyecto')
+    titulo.textContent = proyecto.titulo
+    
+    let tecnologias = document.createElement('div')
+    tecnologias.classList.add('tecnologias-proyecto')
+
+    let enlaces = document.createElement('div')
+    enlaces.classList.add('enlaces')
+
+    for(let tecnologia of proyecto.tecnologias){
+        let span = document.createElement('span')
+        span.textContent = tecnologia
+        tecnologias.appendChild(span)
+    }
+
+    let enlace = document.createElement('a')
+    enlace.classList.add('github-button')
+    enlace.href = proyecto.enlace
+    enlace.target = '_blank'
+    enlace.textContent = 'Repositorio'
+    
+    enlaces.appendChild(enlace)
+
+    article.appendChild(titulo)
+    article.appendChild(tecnologias)
+    article.appendChild(enlaces)
+
+    return article
+}
+
+const contenedorProyectos = document.getElementById('contenedor-proyectos-front')
+
+window.addEventListener('load', () => {
+    proyectos.forEach(proyecto => {
+        contenedorProyectos.appendChild(renderizarProyectos(proyecto))
+    });
+})
